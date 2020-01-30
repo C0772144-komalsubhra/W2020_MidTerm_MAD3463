398345378d200eec2642c712a37f771710716dbb
C0772144-komalsubhra <komalsubhra6768@gmail.com>
2020-01-29 19:46:18 -0500

    (komal subhra) most

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -81,7 +81,7 @@ public class LambtonStringTools
             if(s.equals(0)){
              break;
             }else{
-                int temp = s%10;
+                int temp = s
                 decimal += temp*Math.pow(2, n);
                 s = s/10;
                 n++;
